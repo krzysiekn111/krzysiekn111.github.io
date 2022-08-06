e107eff4d2c8217a2d5ca022f16f985cde0be5a0
krzysiekn111 <krzysiekn111@gmail.com>
2022-08-06 10:40:02 +0200

    wersja aplikacji z modułu

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://krzysiekn111.github.io/';
-  const trelloApiRoot = 'https://kodilla-tasks-kn.herokuapp.com/v1/';
+  const apiRoot = 'http://localhost:8080/v1/tasks';
+  const trelloApiRoot = 'http://localhost:8080/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
