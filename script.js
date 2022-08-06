aadc4d533e8bc4241ba23d1cd5f5524fbdcc5efe
krzysiekn111 <krzysiekn111@gmail.com>
2022-08-06 17:22:31 +0200

    23.1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/tasks';
+  const apiRoot = 'https://kodilla-tasks-kn.herokuapp.com/v1/tasks';
   const trelloApiRoot = 'https://api.trello.com/1';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
